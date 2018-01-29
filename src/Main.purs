d97ba5dc8a70131c3da3a67fa7d06e1a1470ef4c
Roberto Lucha <roberto.lucha@uswitch.com>
2018-01-29 22:05:26 +0000

    pure add on main

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,10 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-newtype Point = Point { x :: Number, y :: Number }
+data Point = Point { x :: Number, y :: Number }
 
 instance showPoint :: Show Point where
-  show (Point p) = show p.x <> "," <> show p.y
+  show (Point p) = show p.x <> " , " <> show p.y
 
 instance semiringPoint :: Semiring Point where
   add = sumPoint
@@ -21,22 +21,13 @@ a = Point {x: 1.0, y: 2.0}
 b :: Point
 b = Point {x: 2.0, y: 3.0}
 
--- getX :: Point -> Number
--- getX {x,y} = x
-
--- getY :: Point -> Number
--- getY {x,y} = y
-
 sumPoint :: Point -> Point -> Point
 sumPoint (Point a) (Point b) = Point { x: a.x + b.x , y: a.y + b.y}
 
 mulPoint :: Point -> Point -> Point
 mulPoint (Point a) (Point b) = Point { x: a.x * b.x , y: a.y * b.y}
 
-main :: Point -> Point -> forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Point -> Point -> Eff (console :: CONSOLE | e) Unit
 main a b = do
-  p <- a + b
-  log "Point is" <> show p
-
--- main :: forall e. Eff (console :: CONSOLE | e) Unit
--- main = log "run"
+  let x =  add a b
+  log (show x)
