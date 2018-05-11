722dc253b460b60c107a281f99f173992292d4e1
Roberto Lucha <roberto.lucha@uswitch.com>
2018-05-11 12:32:37 +0100

    Clenaup

--- src/Main.purs
+++ src/Main.purs
@@ -18,7 +18,7 @@ import Three.Renderer (createWebGLRenderer, setPixelRatio, setSize, mountRendere
 import Three.Camera (createPerspectiveCamera)
 import Three.OrbitControls (OrbitControls, createOrbitControls, enableControls, updateControls)
 
-import Scenes.DotMatrix  as DotMatrix
+import Projects.DotMatrix  as DotMatrix
 
 incT :: Time -> Number
 incT n = toNumber(n + 1) / 100.0
@@ -32,18 +32,18 @@ createRenderer =
     >>= setPixelRatio -- Defaults to device ratio right now
     >>= setSize 1200.0 600.0
 
-initScene :: ThreeT (Tuple Scene DotMatrix.AnimatedScene)
+initScene :: ThreeT (Tuple Scene DotMatrix.Project)
 initScene = do 
   -- this createScene is the ThreeJS new Scene fn, not our actual scene representation
   -- find better naming to make them apart
   scene <- createScene
   color <- createColor "#000000"
   -- Compose this 3 fns below
-  animatedScene <- DotMatrix.create
-  let objects = DotMatrix.getSceneObjects animatedScene
+  project <- DotMatrix.create
+  let objects = DotMatrix.getProjectObjects project
   _ <- addToScene objects scene
   setSceneBackground color scene
-  pure $ Tuple scene animatedScene
+  pure $ Tuple scene project
 
 attachAxesHelper :: Scene -> Number -> ∀ e. Eff (three :: Three | e) Unit
 attachAxesHelper scene size = do
@@ -55,7 +55,7 @@ createControls camera scene = do
   controls <- createOrbitControls camera
   enableControls controls 
 
-updateScene :: forall e. DotMatrix.AnimatedScene -> Camera -> Renderer -> Array Number -> Eff (three :: Three | e) Unit
+updateScene :: forall e. DotMatrix.Project -> Camera -> Renderer -> Array Number -> Eff (three :: Three | e) Unit
 updateScene s c r t = do
 -- Just while developing!! dangerous!
   DotMatrix.update s (unsafePartial $ unsafeIndex t 2)
@@ -64,15 +64,15 @@ updateScene s c r t = do
 -- basically we should declare module effects and doloop should pick those up
 -- and merge them with the default ones...
 -- TODO Provide an interface to run loop with just the custom things
-doLoop :: ∀ e. OrbitControls -> Tuple Scene DotMatrix.AnimatedScene -> Camera -> Renderer -> Eff (three :: Three, console :: CONSOLE | e) Unit
+doLoop :: ∀ e. OrbitControls -> Tuple Scene DotMatrix.Project -> Camera -> Renderer -> Eff (three :: Three, console :: CONSOLE | e) Unit
 doLoop controls (Tuple s as) camera renderer = makeLoop
   -- Caculations (should be partially applied to be useful to the scene!)
     [ id <<< toNumber
     , incT
     , cosT ]
   -- Time bound effects
-    [log <<< show
-    , updateScene as camera renderer
+    [ --log <<< show
+      updateScene as camera renderer
     ]
   -- Time free effects
     [ updateControls controls
@@ -93,8 +93,7 @@ main = do
   doLoop controls scene camera renderer
 
 -- TODO:
--- Change name of animatedScene to something that makes it apart por threejs scene
--- Make animatedScene a graph and provide a way to traverse it
+-- Make Project a graph and provide a way to traverse it
 -- Make results a record and provide a way to hook propery results animtedScene Update functions
 -- Remove all partial unsafe functions
 -- Make doLoop actually beautiful
@@ -104,7 +103,7 @@ main = do
 -- Create a set of JS utils to make IFF less painful
 -- Get canvas size from window size
 -- Change it on resize, updateMatrices
--- Remove Tuple Scene DotMatrix.AnimatedScene everywhere and treat them separatedly
+-- Remove Tuple Scene DotMatrix.Project everywhere and treat them separatedly
 -- Add Camera position set and camera lookAt fns
 -- Create a draft of a type structure for threeJS (Object3D, Material, etc.)
 -- Orbitcontrol rotate, autoupdate fns, enable zoom, etc.
--- src/time/Loop.purs
+++ src/time/Loop.purs
@@ -3,15 +3,11 @@ module Time.Loop where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Foldable (sequence_)
-import Data.Traversable (traverse)
+import Data.Traversable (traverse_)
 
-foreign import setAnimationFrameBehaviour 
-  :: forall eff. (Eff eff) Unit -> (Eff eff) Unit
+foreign import setAnimationFrameBehaviour :: forall eff. (Eff eff) Unit -> (Eff eff) Unit
 
-type Time = Int 
-
-data State = State { x :: Number }
-
+newtype Time = Time Int
 -- recommendation to use newtype
 -- newtype LoopCalculation = LoopCalculation (Time -> Number)
 type LoopCalculation = Time -> Number
@@ -21,21 +17,32 @@ type LoopCalculation = Time -> Number
 type LoopBehaviour e = Array Number -> Eff e Unit
 type LoopEffect e = Eff e Unit
 
+runCalculations :: Array LoopCalculation -> Time -> Array Number 
+runCalculations calcs t = calcs <*> [t]
+
+runBehaviours :: forall e. Array Number -> Array (LoopBehaviour e) -> Eff e Unit
+runBehaviours rs bs = traverse_ (\f -> f rs) bs
+
+runEffects :: forall e. Array (LoopEffect e) -> Eff e Unit
+runEffects effs = sequence_ effs
+
+increaseTime :: Time -> Int -> Time
+increaseTime (Time t) i = Time (t + i)
+
 makeLoop 
   :: forall e. 
   Array LoopCalculation ->
   Array (LoopBehaviour e) ->
   Array (LoopEffect e) ->
-  -- State ->
   Time ->
   Eff e Unit
+makeLoop cs bs effs t = do
+  runBehaviours results bs
+  runEffects effs
+  setAnimationFrameBehaviour $ makeLoop cs bs effs inc
+  where results = runCalculations cs t
+        inc = increaseTime t 1
 
--- makeLoop calcs behs effs st t = do
-makeLoop calcs behs effs t = do
-  let results = calcs <*> [t] -- run calculations over time
-  _ <- traverse (\f -> f results) behs  -- execute time bound effects
-  sequence_ effs                  -- execute time free effects
-  setAnimationFrameBehaviour $ makeLoop calcs behs effs (t+1) -- recurse
 
 -- makeLoop will take eventually another param which is the results of traversing all the computations
 
@@ -44,3 +51,4 @@ makeLoop calcs behs effs t = do
 -- If you're not familiar with Free Monads, 
 -- maybe it's worth watching @natefaubion’s excellent presentation (https://www.youtube.com/watch?v=eKkxmVFcd74) and playing with the idea. 
 -- Perhaps even a look into `purescript-run` is worth it. (edited)
+
