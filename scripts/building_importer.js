9db4cfb2fa03acca98549a384f430dcd83c85841
Roberto Lucha <roberto.lucha@uswitch.com>
2018-06-05 18:09:34 +0100

    psotcode surroundings

--- scripts/building_importer.js
+++ scripts/building_importer.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var buildings = require("./data.json")
+var buildings = require("./se228np_1km.json")
 var results = JSON.stringify(buildings.map (b=> ({coordinates: JSON.parse(b.points).map(p => ({x: p[0], y: 0.0, z: p[1]}))})))
 
-fs.writeFile("./buildings.json", results , function(err) {
+fs.writeFile("./buildings_se228np_1km.json", results , function(err) {
     if(err) {
         return console.log(err);
     }
