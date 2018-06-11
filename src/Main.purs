e0cf06f840946ca71570237568a6a0363f861371
rlucha <rlucha@ZPG-C02VR20RHTDD.local>
2018-06-11 12:25:30 +0100

    Use psc-package to force purs 0.11.7

--- src/Main.purs
+++ src/Main.purs
@@ -1,4 +1,4 @@
-module Test.Main where
+module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
@@ -6,4 +6,4 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "You should add some tests."
+  log "Hello sailor!"
